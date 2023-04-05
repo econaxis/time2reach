1620f2d5f6ec9310e496a83c6b23349dbe58f60d
Henry Nguyen <henry.nguyen@super.com>
2023-04-05 00:37:19 -0400

    add tailwind

---
+++ web/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index1.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
