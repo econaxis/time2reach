df4b2e3336806b3e0a459bee8a33618f69d6f6c3
henry <henry2833@gmail.com>
2023-07-06 16:42:17 -0400

    deploy changes

--- deploy/dbuild.sh
+++ deploy/dbuild.sh
@@ -2,7 +2,10 @@
 
 set -e
 source deploy/.env.prod
+
+
+docker login ghcr.io -u USERNAME -p $DOCKER_WRITE
+
 docker build -t ghcr.io/econaxis/test . && docker push ghcr.io/econaxis/test
 
-echo $GITHUB_PAT
 ssh 35.238.190.254 -t "sh setup-docker-on-host.sh $GITHUB_PAT; exec bash -l"
