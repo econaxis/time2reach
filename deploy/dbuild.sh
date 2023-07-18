9e7083b79333738c8bb4649875bc08d82c54a4a3
henry <henry2833@gmail.com>
2023-07-18 16:45:49 -0400

    add github actions

--- deploy/dbuild.sh
+++ deploy/dbuild.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-source deploy/.env.prod
-#set -e
-
-
-if [[ -z $GITHUB_ACTIONS ]]; then
-  docker login ghcr.io -u USERNAME -p $DOCKER_WRITE
-  rsync -rvahz --progress --relative ./city-gtfs/ ./web/public/ ./deploy/ ./certificates/ 35.238.190.254:data2/
-  docker build -t ghcr.io/econaxis/test .
-  docker push ghcr.io/econaxis/test
-
+if [[-z REMOTE_BUILD ]]; then
+else
+  source deploy/.env.prod
 fi
 
-sleep 0.1
+docker login ghcr.io -u USERNAME -p GITHUB_PAT
+docker build -t ghcr.io/econaxis/test .
+docker push ghcr.io/econaxis/test
+if [[ -z REMOTE_BUILD ]]; then
+  sh ~/data2/deploy/setup-docker-on-host.sh $GITHUB_PAT
+else
+  rsync -rvahz --progress --relative ./city-gtfs/ ./web/public/ ./deploy/ ./certificates/ 35.238.190.254:data2/
+  sleep 0.1
+  ssh -o StrictHostKeyChecking=no 35.238.190.254 -t "pwd; sh data2/deploy/setup-docker-on-host.sh $GITHUB_PAT; exec bash -l"
+fi
 
-ssh -o StrictHostKeyChecking=no 35.238.190.254 -t "pwd; sh data2/deploy/setup-docker-on-host.sh $GITHUB_PAT; exec bash -l"
